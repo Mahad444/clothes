075b49ee6bc00646f3e0736dfc4d8c7858ec0c20
Ally Juma Said <mahadsaid95@icloud.com>
2023-12-03 23:20:32 +0000

    hard

--- clothing-ecommerce/app/components/Navbar.tsx
+++ clothing-ecommerce/app/components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = (props: Props) => {
         <div className="flex items-center md:space-x-10 lg:space-x-20">
           {/* logo name  */}
           <div className="font-semibold text-2xl">
-            <a href="/">Modestified</a>
+            <a href="/">Modestified ClothLine</a>
           </div>
           <nav className="max-md:hidden">
             <ul className=" flex items-center lg:space-x-10 space-x-7 opacity-70 text-[15px]">
